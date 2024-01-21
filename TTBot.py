957c5d8a304896b9e60a413a2b432317928889a4
ikboljon1 <ikboljonuldashvaev@gmail.com>
2024-01-20 22:31:52 -0500

    Added context saving feature

--- TTBot.py
+++ TTBot.py
@@ -13,33 +13,14 @@ openai.api_key = OPENAI_API_KEY
 
 bot = telebot.TeleBot("6853169439:AAHZdBoHQwWroofKKqNPD3j9VVRsEZSgTfE")
 
-BOT_ROLE = "customer support bot"
-task = "HoneyMoon mahsuloti haqida berilga savolga aniq va to'liq javob ber "
+BOT_ROLE = "HoneyMoon махсулоти хакида малумот берувчи чатбот"
 user_contexts = {}
 chat_log = []
 CHAT_LOG_DIR = "chat_logs"
 file = 'Data/HoneyMoon.txt'
 loader = TextLoader(file)
-# info_loader = TextLoader('Data/info.txt')
-# price_loader = TextLoader('Data/price.txt')
-# composition_loader = TextLoader('Data/composition.txt')
-# benefits_loader = TextLoader('Data/benefits.txt')
-# illnesses_loader = TextLoader('Data/illnesses.txt')
-# usage_loader = TextLoader('Data/usage.txt')
-# delivery_loader = TextLoader('Data/delivery.txt')
-#
-# loaders = [info_loader, price_loader,composition_loader,benefits_loader,illnesses_loader,usage_loader,delivery_loader]
-# docs = []
-# # Ҳар бир лоадердан маълумотларни юклаймиз
-# for l in loaders:
-#     docs.extend(l.load())
-
 # Vectorstore index
 index = VectorstoreIndexCreator().from_loaders([loader])
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Assalomu alaykum, men sizga HoneyMoonnig foydasi, tarkibi, narhlari, istemoli va boshqa savollaringizga javob berishga harakat qilaman!")
 
 @bot.message_handler(func=lambda msg: True)
 def handle_message(msg):
@@ -54,14 +35,14 @@ def handle_message(msg):
 
   context = user_contexts[user_id]
 
-  if len(context) > 10:
-    context = context[-10:]
+  if len(context) > 5:
+    context = context[-5:]
 
   user_file = os.path.join(CHAT_LOG_DIR, f"{user_name}_{user_id}.txt")
 
   if not os.path.exists(CHAT_LOG_DIR):
     os.mkdir(CHAT_LOG_DIR)
-  prompt = f"Bot role: {BOT_ROLE} \n\nTask:{task}\n\n User query: {query} \n\nhistory:{context}"
+  prompt = f"Bot role: {BOT_ROLE}\\n\\nUser query: {query}\\nPrevious context:\\n{context}\\n\\nBot response:"
   print(prompt)
 
 
@@ -79,7 +60,7 @@ def handle_message(msg):
     f.write(f"{ 'BOT' }: {result['answer']}\n\n")
 
   chat_log.append((query, result['answer']))
-  context.append(query)
+
   context.append(result['answer'])
   user_contexts[user_id] = context
 
@@ -99,9 +80,4 @@ def load_contexts(file_path):
         with open(file_path, 'r') as file:
             return json.load(file)
     except FileNotFoundError:
-        return {}  # Return an empty dictionary if the file does not exist
-
-# Example usage
-# user_contexts = load_contexts('user_contexts.json')
-# ... update user_contexts during the bot operation ...
-# save_contexts(user_contexts, 'user_contexts.json')
+        return {}
